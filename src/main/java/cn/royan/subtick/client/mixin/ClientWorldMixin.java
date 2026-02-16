6b24d705eac16c00e8566bf1ff6a8fa5c740d5c0
RoyanAB <2275950121@qq.com>
2026-02-16 09:58:30 +0800

    ClientWorldMixin

--- src/main/java/cn/royan/subtick/client/mixin/ClientWorldMixin.java
+++ src/main/java/cn/royan/subtick/client/mixin/ClientWorldMixin.java
@@ -1,6 +1,8 @@
 package cn.royan.subtick.client.mixin;
 
 import cn.royan.subtick.client.ClientTickHandler;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import net.minecraft.client.world.ClientWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,5 +19,31 @@ public class ClientWorldMixin {
 	)
 	private void onTick(CallbackInfo ci) {
 		ClientTickHandler.onTick((ClientWorld) (Object) this);
+	}
+
+	@WrapWithCondition(
+		method = "tick",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/world/ClientWorld;setTime(J)V"
+		)
+	)
+	public boolean wrapClientWorldTimeUpdate(ClientWorld instance, long l) {
+		return !ClientTickHandler.frozen;
+	}
+
+	@ModifyExpressionValue(
+		method = "tick",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/world/GameRules;getBoolean(Ljava/lang/String;)Z",
+			ordinal = 0
+		)
+	)
+	private boolean wrapClientDayTimeUpdate(boolean original) {
+		if (!ClientTickHandler.frozen) {
+			return original;
+		}
+		return false;
 	}
 }
