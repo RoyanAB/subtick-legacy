d79237e0d5a89d0fd88c22b2e2e0ecffc6e40965
RoyanAB <2275950121@qq.com>
2026-02-17 12:55:23 +0800

    gen2???

--- src/main/java/cn/royan/subtick/SubtickMod.java
+++ src/main/java/cn/royan/subtick/SubtickMod.java
@@ -1,7 +1,5 @@
 package cn.royan.subtick;
 
-import carpet.commands.CarpetAbstractCommand;
-import cn.royan.subtick.setting.CarpetSettings;
 import cn.royan.subtick.setting.Settings;
 import cn.royan.subtick.utils.Translations;
 import cn.royan.subtick.utils.deobfuscator.StackTraceDeobfuscator;
@@ -19,7 +17,7 @@ public class SubtickMod implements ModInitializer {
 	public static String MOD_VERSION = "unknown";
 	public static String MOD_NAME = "unknown";
 
-	public static final boolean hasCarpet = FabricLoader.getInstance().isModLoaded("carpet");
+//	public static final boolean hasCarpet = FabricLoader.getInstance().isModLoaded("carpet");
 
 	public static Settings settings;
 
@@ -32,66 +30,61 @@ public class SubtickMod implements ModInitializer {
 		settings = Settings.loadConfig(FabricLoader.getInstance().getConfigDir().resolve("subtick.json"));
 		StackTraceDeobfuscator.fetchMapping();
 
-		if (!hasCarpet) {
+//		if (!hasCarpet)
 			Translations.getTranslationFromResourcePath(settings.language);
-		}
-	}
-
-	public void preLaunch() {
-		if (hasCarpet) SubtickCarpetExtension.loadExtension();
 	}
 
 	public static boolean tickCommand(CommandSource commandSource, String name) {
-		if (hasCarpet)
-			return CarpetAbstractCommand.canUseCommand(commandSource, CarpetSettings.tickCommand);
+//		if (hasCarpet)
+//			return CarpetAbstractCommand.canUseCommand(commandSource, CarpetSettings.tickCommand);
 		return commandSource.canUseCommand(2, name);
 	}
 
 	public static String subtickDefaultPhase() {
-		if (hasCarpet)
-			return CarpetSettings.subtickDefaultPhase;
+//		if (hasCarpet)
+//			return CarpetSettings.subtickDefaultPhase;
 		return settings.subtickDefaultPhase;
 	}
 
 	public static String subtickTextFormat() {
-		if (hasCarpet)
-			return CarpetSettings.subtickTextFormat;
+//		if (hasCarpet)
+//			return CarpetSettings.subtickTextFormat;
 		return settings.subtickTextFormat;
 	}
 
 	public static String subtickNumberFormat() {
-		if (hasCarpet)
-			return CarpetSettings.subtickNumberFormat;
+//		if (hasCarpet)
+//			return CarpetSettings.subtickNumberFormat;
 		return settings.subtickNumberFormat;
 	}
 
 	public static String subtickPhaseFormat() {
-		if (hasCarpet)
-			return CarpetSettings.subtickPhaseFormat;
+//		if (hasCarpet)
+//			return CarpetSettings.subtickPhaseFormat;
 		return settings.subtickPhaseFormat;
 	}
 
 	public static String subtickDimensionFormat() {
-		if (hasCarpet)
-			return CarpetSettings.subtickDimensionFormat;
+//		if (hasCarpet)
+//			return CarpetSettings.subtickDimensionFormat;
 		return settings.subtickDimensionFormat;
 	}
 
 	public static String subtickErrorFormat() {
-		if (hasCarpet)
-			return CarpetSettings.subtickErrorFormat;
+//		if (hasCarpet)
+//			return CarpetSettings.subtickErrorFormat;
 		return settings.subtickErrorFormat;
 	}
 
 	public static int subtickDefaultRange() {
-		if (hasCarpet)
-			return CarpetSettings.subtickDefaultRange;
+//		if (hasCarpet)
+//			return CarpetSettings.subtickDefaultRange;
 		return settings.subtickDefaultRange;
 	}
 
 	public static boolean yeetCrash() {
-		if (hasCarpet)
-			return CarpetSettings.yeetCrash;
+//		if (hasCarpet)
+//			return CarpetSettings.yeetCrash;
 		return settings.subtickYeetCrash;
 	}
 
